e6949f7f791aa0d3a6775d4455fbab252564771d
coaxfiber <coaxfiber05@gmail.com>
2018-02-11 22:26:31 +0800

    2.11.185

--- gs_scheduling (1).sql
+++ gs_scheduling (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2018 at 09:57 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: Feb 11, 2018 at 03:25 PM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -69,8 +69,16 @@ CREATE TABLE `gs_faculty` (
   `fname` varchar(50) NOT NULL,
   `mname` varchar(50) NOT NULL,
   `lname` varchar(50) NOT NULL,
-  `status` varchar(10) NOT NULL
+  `status` varchar(10) NOT NULL,
+  `progname` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gs_faculty`
+--
+
+INSERT INTO `gs_faculty` (`id`, `idno`, `ext`, `fname`, `mname`, `lname`, `status`, `progname`) VALUES
+(1, '22', 'MR.', '2', '22', '22', 'Inactive', 'MIT');
 
 -- --------------------------------------------------------
 
@@ -92,7 +100,7 @@ INSERT INTO `gs_option` (`id`, `gsoption`, `value`) VALUES
 (1, 'Dean', 'INICIA C. BANSIG, Ph.D.'),
 (2, 'vp', 'AGRIPINA MARIBBAY, Ph.D.'),
 (3, 'active_year', '2017-2018'),
-(4, 'active_term', 'third semester');
+(4, 'active_term', 'Third Semester');
 
 -- --------------------------------------------------------
 
@@ -113,7 +121,7 @@ CREATE TABLE `gs_program` (
 
 INSERT INTO `gs_program` (`id`, `short`, `program`, `specialization`) VALUES
 (21, 'MIT', 'Master in Information Technology', 'System Development'),
-(22, 'asd', 'asd', 'wwww');
+(22, 'MIT', 'Master in Information Technology', 'Information Management');
 
 -- --------------------------------------------------------
 
@@ -154,8 +162,17 @@ CREATE TABLE `gs_scheduling` (
   `professor` varchar(150) NOT NULL,
   `term` varchar(20) NOT NULL,
   `year` varchar(9) NOT NULL,
-  `programid` int(11) NOT NULL
+  `programid` int(11) NOT NULL,
+  `start` date NOT NULL,
+  `position` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `gs_scheduling`
+--
+
+INSERT INTO `gs_scheduling` (`id`, `code`, `title`, `units`, `sched`, `room`, `professor`, `term`, `year`, `programid`, `start`, `position`) VALUES
+(1, 'mit', '123', 3, '123', '123', '1231', '12312', '231', 123, '2018-02-13', 0);
 
 -- --------------------------------------------------------
 
@@ -178,8 +195,14 @@ CREATE TABLE `gs_subject` (
 --
 
 INSERT INTO `gs_subject` (`id`, `code`, `title`, `units`, `remarks`, `program`, `type`) VALUES
-(1, 'INS 201', 'Pauline Ethics', 3, 'for Non-Paulinian Graduate', 0, 'Institutional Course'),
-(4, 'MIT 301', 'advance Operating Systems and Networking', 3, '', 21, 'Major Courses');
+(1, 'INS 201', 'Pauline Ethics', 3, 'for Non-Paulinian Graduate', 0, 'Institutional Courses'),
+(4, 'MIT 301', 'Advance Operating Systems and Networking', 3, '', 21, 'Core Courses'),
+(6, 'MIT 302', 'Advance Database System', 3, '', 21, 'Core Courses'),
+(7, 'MIT 303', 'Advanced Systems Design and Implementation', 3, '', 21, 'Core Courses'),
+(8, 'MIT 304', 'Global Information Technology &amp; Project Management', 3, '', 21, 'Core Courses'),
+(9, 'MIT_SD 401', 'Advanced Software Enginnering', 3, '', 21, 'Major Courses'),
+(10, 'MIT_SD 402', 'Advanced Systems Analysis and Design', 3, '', 21, 'Major Courses'),
+(11, 'MITCP 01', 'Capstone Project 1', 6, '', 21, 'Independent Projects');
 
 --
 -- Indexes for dumped tables
@@ -242,49 +265,41 @@ ALTER TABLE `gs_subject`
 --
 ALTER TABLE `gs_account`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `gs_building`
 --
 ALTER TABLE `gs_building`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
 --
 -- AUTO_INCREMENT for table `gs_faculty`
 --
 ALTER TABLE `gs_faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `gs_option`
 --
 ALTER TABLE `gs_option`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
 --
 -- AUTO_INCREMENT for table `gs_program`
 --
 ALTER TABLE `gs_program`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
-
 --
 -- AUTO_INCREMENT for table `gs_rooms`
 --
 ALTER TABLE `gs_rooms`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
 --
 -- AUTO_INCREMENT for table `gs_scheduling`
 --
 ALTER TABLE `gs_scheduling`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `gs_subject`
 --
 ALTER TABLE `gs_subject`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
